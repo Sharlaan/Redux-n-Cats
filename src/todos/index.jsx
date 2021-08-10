e0c186e8c65bd0f2eedbb89001b08c9991d82ffb
Sharlaan <raphael.morineau@gmail.com>
2021-08-10 21:48:11 +0200

    Update dependencies and fixes

--- src/todos/index.jsx
+++ src/todos/index.jsx
@@ -3,9 +3,11 @@ import TodosForm from './todos.form';
 import Todos from './todos.list';
 import styles from './todos.module.css';
 
-export default () => (
-  <article className={styles.main}>
-    <TodosForm />
-    <Todos />
-  </article>
-);
+export default function TodosSection() {
+  return (
+    <article className={styles.main}>
+      <TodosForm />
+      <Todos />
+    </article>
+  );
+}
